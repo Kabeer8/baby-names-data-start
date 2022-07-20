22b0bc7ea35ef3f5f0f45d97177b1d127ecbb20a
Kabeer8 <108748863+Kabeer8@users.noreply.github.com>
2022-07-20 11:12:10 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Kabeer's copy
 // Baby Names Data (Top 50 Boy/Girl 2022)
 // Baby Center (babycenter.com)
 // https://www.babycenter.com/baby-names/most-popular/top-baby-names#popularNameList
@@ -38,27 +39,85 @@ function goBtnClicked() {
 
 // Display All Baby Names
 function displayAll() {
-  console.log("Display All");
-  // Confirm data load
-  console.log(babyData);
+  nameCountSpan.innerHTML = "";
+  let all = "";
+  for (let i = 0; i < babyData.length; i++){
+    all += babyProfile(babyData[i]);
+  }
+  container.innerHTML = all;
+  nameCountSpan.innerHTML = babyData.length;
 }
 
 // Display Names by Gender
 function searchGender() {
+  nameCountSpan.innerHTML = "";
   console.log("Search By Gender");
+  let gender = prompt("Which gender? (enter Boy or Girl)")
+  let text = "";
+  let counter = 0;
+  for (let i = 0; i < babyData.length; i++){
+    if(gender == babyData[i].gender){
+      text += babyProfile(babyData[i]);
+      counter += 1
+    }
+  }
+  container.innerHTML = text;
+  nameCountSpan.innerHTML = counter;
+
 }
 
 // Display Names within a Range of Ranks
 function searchRank() {
-  console.log("Search By Rank");
+  nameCountSpan.innerHTML = "";
+  let counter = 0
+  let min = prompt("Please enter a minimum rank:")
+  let max = prompt("Please enter a maximum rank:")
+  let text =""
+  for (let i = 0; i < babyData.length; i++){
+    if(babyData[i].rank >= min && babyData[i].rank <= max){
+      text += babyProfile(babyData[i]);
+      counter += 1
+    }
+  }
+  container.innerHTML = text;
+  nameCountSpan.innerHTML = counter;
 }
 
 // Display Names with Starting Letter
 function searchStartingLetter() {
-  console.log("Search by Starting Letter");
+  let userLetter = prompt("Enter a letter (capitilized):");
+  let text = "";
+  nameCountSpan.innerHTML = "";
+  let counter = 0;
+  for (let i = 0; i < babyData.length; i++){
+    if(babyData[i].name.startsWith(userLetter)){
+      text += babyProfile(babyData[i]);
+      counter += 1
+    }
+  }
+  container.innerHTML = text;
+  nameCountSpan.innerHTML = counter;
 }
 
 // Display Names with a Specific Length
 function searchLength() {
-  console.log("Search by Name Length");
+  let userLength = prompt("Enter a name length:");
+  let text = "";
+  nameCountSpan.innerHTML = "";
+  let counter = 0;
+  for (let i = 0; i < babyData.length; i++){
+    if(babyData[i].name.length == userLength){
+      text += babyProfile(babyData[i]);
+      counter += 1
+    }
+  }
+  container.innerHTML = text;
+  nameCountSpan.innerHTML = counter;
+}
+
+function babyProfile(baby){
+  return `
+  <div>
+  <span class="bolded">${baby.name}</span> (Rank: ${baby.rank}, Gender: ${baby.gender}) 
+  </div>`;
 }
